ffc4c832e318832d7b3315641171ab1d0905daf3
barry <barryPan12138@outloot.com>
2025-03-14 13:43:19 +0800

    add NumberCodeController

---
+++ online-taxi-public/service-verificationCode/src/main/java/com/pansiyi/serviceverificationcode/controller/NumberCodeController.java
@@ -0,0 +1,26 @@
+package com.pansiyi.serviceverificationcode.controller;
+
+import com.pansiyi.internalcommon.dto.ResponseResult;
+import com.pansiyi.internalcommon.response.NumberCodeResponse;
+import net.sf.json.JSONObject;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class NumberCodeController {
+
+    @GetMapping("/numberCode/{size}")
+    public ResponseResult NumberCode(@PathVariable int size){
+
+        System.out.println("size:"+size);
+        double mathRandom = (Math.random() * 9 + 1) * Math.pow(10, size - 1);
+        int numberCode = (int)mathRandom;
+
+        NumberCodeResponse response = new NumberCodeResponse();
+        response.setNumberCode(numberCode);
+
+        return ResponseResult.success(response);
+
+    }
+}
